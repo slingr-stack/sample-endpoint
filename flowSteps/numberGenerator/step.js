58dc509eabc266460da023004ac63c041108eded
smoyano <smoyano@slingr.io>
2022-01-14 15:05:11 -0300

    Fixed issue executing flow script.

--- flowSteps/numberGenerator/step.js
+++ flowSteps/numberGenerator/step.js
@@ -4,5 +4,5 @@
  * @param {number} bound, This is used to get a random number between 0 (inclusive) and the number passed in this argument, exclusive.
  */
 step.numberGenerator = function (bound) {
-    return endpoint.randomNumber({bound});
+    return endpoint.randomNumber({bound: bound});
 };
