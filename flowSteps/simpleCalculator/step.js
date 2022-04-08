94c5502586bf5a783757a809709ef9800a9deb5a
Juan Orbegozo <juaniorbegozo@gmail.com>
2022-04-08 15:12:33 -0300

    fix steps

--- flowSteps/simpleCalculator/step.js
+++ flowSteps/simpleCalculator/step.js
@@ -6,22 +6,22 @@
  * @param {number} secondOperand, Second operand
  */
 step.simpleCalculator = function (operator, firstOperand, secondOperand) {
-/*
+
     let result = 0;
     switch (operator) {
         case 'SUM':
-            result = endpoint.sum(firstOperand, secondOperand);
+            result = endpoint.sum({firstOperand: firstOperand}, {secondOperand: secondOperand});
             break;
         case 'REST':
-            result = endpoint.rest(firstOperand, secondOperand);
+            result = endpoint.rest({firstOperand: firstOperand}, {secondOperand: secondOperand});
             break;
         case 'DIV':
-            result = endpoint.div(firstOperand, secondOperand);
+            result = endpoint.div({firstOperand: firstOperand}, {secondOperand: secondOperand});
             break;
         case 'MULT':
-            result = endpoint.mult(firstOperand, secondOperand);
+            result = endpoint.mult({firstOperand: firstOperand}, {secondOperand: secondOperand});
             break;
     }
-    */
-    return endpoint.rnd();
+
+    return result;
 };
