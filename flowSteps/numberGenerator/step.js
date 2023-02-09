6def52167597fc551461ed5de8927282d3b4ec70
smoyano <smoyano@slingr.io>
2023-02-09 09:45:54 -0300

    #13407 Refactor available variable in flow step config in endpoints

--- flowSteps/numberGenerator/step.js
+++ flowSteps/numberGenerator/step.js
@@ -1,8 +1,8 @@
 /**
  * Generates a random number.
- *
- * @param {number} bound, This is used to get a random number between 0 (inclusive) and the number passed in this argument, exclusive.
+ * @param {object} inputs
+ * @param {inputs.number} bound, This is used to get a random number between 0 (inclusive) and the number passed in this argument, exclusive.
  */
-step.numberGenerator = function (bound) {
-    return endpoint.randomNumber({bound: bound});
+step.numberGenerator = function (inputs) {
+    return endpoint.randomNumber({bound: inputs.bound});
 };
